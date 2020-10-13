c8266bdfe39c16005a77d466b439adb20a952cb0
GustavoDeveloper1 <devinvestidor02@gmail.com>
2020-10-13 13:55:18 -0300

    adição de icones e estização da landing page e da page map

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/global.css';
+import './styles/pages/page-landing.css';
+
+import logoimg from './images/Logo.svg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page-landing">
+        <div className="content-wrapper">
+          <img src={logoimg} alt="Happy"/>
+
+          <main>
+            <h1>Leve Felicidade para o mundo</h1>
+            <p>Visite orfanatos e mude o dia
+de muitas crianças.</p>
+          </main>
+
+          <div className="location">
+            <strong>Brasilia</strong>
+            <span>Distrito Federal</span>
+          </div>
+
+          <a href="" className="enter-app">
+            >
+          </a>
+
+
+        </div>
     </div>
   );
 }
